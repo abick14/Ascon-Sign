22a46f98d68126f5d7fb386709f71e86d1562805
abick14 <119895377+abick14@users.noreply.github.com>
2023-12-11 13:41:54 -0500

    Update and rename SphincsPlusKeyParameters.cs to AsconSignKeyParameters.cs

---
+++ AsconSignKeyParameters.cs
@@ -0,0 +1,18 @@
+﻿using Org.BouncyCastle.Crypto;
+
+namespace Org.BouncyCastle.Pqc.Crypto.AsconSign
+{
+    public abstract class AsconSignKeyParameters
+        : AsymmetricKeyParameter
+    {
+        private readonly AsconSignParameters m_parameters;
+
+        internal AsconSignKeyParameters(bool isPrivate, AsconSignParameters parameters)
+            : base(isPrivate)
+        {
+            m_parameters = parameters;
+        }
+
+        public AsconSignParameters Parameters => m_parameters;
+    }
+}
